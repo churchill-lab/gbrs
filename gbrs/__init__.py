a07b67bfa3ab2b319d242c8a95a1950b35a6ae37
KB Choi <kwangbom@gmail.com>
2016-02-18 18:54:08 -0500

    [kbchoi] Created gbrs main command. Implemented 'reconstruct' subcommand.

--- gbrs/__init__.py
+++ gbrs/__init__.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from .commands import plot
+from .emase_utils import hybridize, align, bam2emase, quantify
 
 __author__ = 'Kwangbom "KB" Choi, Ph.D.'
 __email__ = 'kb.choi@jax.org'
---
+++ gbrs/emase_utils.py
@@ -0,0 +1,17 @@
+import emase
+
+
+def hybridize(**kwargs):
+    pass
+
+
+def align(**kwargs):
+    pass
+
+
+def bam2emase(**kwargs):
+    pass
+
+
+def quantify(**kwargs):
+    pass
