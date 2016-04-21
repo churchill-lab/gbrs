134526d45d80b57fa2ec7c9ba95665d3afeb622d
kbchoi-jax <kb.choi@jax.org>
2016-04-21 15:12:56 -0400

    Subcommand 'mask' is done. Adding 'quantify'.

--- gbrs/emase_utils.py
+++ gbrs/emase_utils.py
@@ -17,16 +17,25 @@ def bam2emase(**kwargs):
 
 
 def mask(**kwargs):
-    alnfile = kwargs.get('alnfile')
-    grpfile = kwargs.get('grpfile')
-    gtypefile = kwargs.get('gtypefile')
+    """
+    Applying genotype calls to multi-way alignment incidence matrix
 
+    :param alnfile: alignment incidence file (h5),
+    :param gtypefile: genotype calls by GBRS (npz),
+    :param grpfile: gene ID to isoform ID mapping (tsv)
+    :return: masked version of alignment incidence file (h5)
+    """
+    alnfile = kwargs.get('alnfile')
     alnmat = emase.AlignmentPropertyMatrix(h5file=alnfile)
+
+    gtypefile = kwargs.get('gtypefile')
     gtype_chr = np.load(gtypefile)
-    gidfile = os.path.join(DATA_DIR, 'gene_ids.npz')
+
+    gidfile = os.path.join(DATA_DIR, 'gene_ids.ordered.npz')
     gname_chr = np.load(gidfile)
 
     g2t = dict()
+    grpfile = kwargs.get('grpfile')
     with open(grpfile) as fh:
         for curline in fh:
             item = curline.rstrip().split()
@@ -47,14 +56,28 @@ def mask(**kwargs):
             gtmask[np.meshgrid(hid2set, tid2set)] = 1.0
     alnmat.multiply(gtmask, axis=2)
 
+    outfile = kwargs.get('outfile')
+    alnmat.save(h5file=outfile)
+
 def quantify(**kwargs):
+    """
+    Quantify expected read counts
+
+    :param:
+    :param:
+    :param:
+    :param:
+    :param:
+    :param:
+    :param:
+    :param:
+    :param:
+    :param:
+    :param:
+    :return:
+    """
     alnfile = kwargs.get('alnfile')
-    gtypefile = kwargs.get('gtypefile')
-    if gtypefile is not None:
-        alnmat = mask(alnmat, **kwargs)
-    else:
-        alnmat = emase.AlignmentPropertyMatrix(h5file=alnfile)
-    pass
+    alnmat = emase.AlignmentPropertyMatrix(h5file=alnfile)
 
 
 
