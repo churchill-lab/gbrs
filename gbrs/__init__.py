0316b269cdc8c64ec54f95e9b4a35e0854b5c590
Mike Lloyd <mike.lloyd@jax.org>
2023-05-03 11:31:36 -0400

    add init to gbrs

---
+++ gbrs/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+from .commands import plot
+from .emase_utils import hybridize, align, bam2emase, quantify
+
+__author__ = 'Kwangbom "KB" Choi, Ph.D.'
+__email__ = ''
+__version__ = '0.2.0'
