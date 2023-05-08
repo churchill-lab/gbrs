b1157163f0d27605bf96757bad7cb7911c7fee9d
mattjvincent <matt.vincent@jax.org>
2023-05-08 01:59:27 -0400

    Multiple cleanups and moves to utilize EMASE and not duplicate everything

--- gbrs/commands.py
+++ gbrs/commands.py
@@ -27,68 +27,6 @@ logging.basicConfig(
 app = typer.Typer()
 
 
-@app.command()
-def hybridize(
-    fasta_list: Annotated[list[Path], typer.Option('-F', '--target-files', exists=True, dir_okay=False, resolve_path=True)],
-    hap_list: Annotated[str, typer.Option('-s', '--suffices')],
-    out_file: Annotated[Path, typer.Option('-o', '--output', exists=False, dir_okay=False, writable=True, resolve_path=True)] = 'gbrs.hybridized.targets.fa',
-    build_bowtie_index: bool = False,
-    verbose: Annotated[int, typer.Option('-v', '--verbose', count=True, help="specify multiple times for more verbose output")] = 0
-) -> None:
-    logger = gbrs_utils.configure_logging(verbose)
-    logger.debug('hybridize')
-    try:
-        emase_utils.hybridize(
-            fasta_list=fasta_list,
-            hap_list=hap_list,
-            out_file=str(out_file),
-            build_bowtie_index=build_bowtie_index
-        )
-    except Exception as e:
-        if logger.level == logging.DEBUG:
-            logger.exception(e)
-        else:
-            logger.error(e)
-
-
-@app.command(help="convert a BAM file to EMASE format")
-def bam2emase(
-    alignment_file: Annotated[Path, typer.Option('-i', '--alignment-file', exists=True, dir_okay=False, resolve_path=True, help="bam file to convert")],
-    haplotypes: Annotated[list[str], typer.Option('-h', '--haplotype-char', help='haplotype, either one per -h option, i.e. -h A -h B -h C, or a shortcut -h A,B,C')],
-    locusid_file: Annotated[Path, typer.Option('-m', '--locus-ids', exists=True, dir_okay=False, resolve_path=True, help='filename for the locus (usually transcripts) info')] = None,
-    out_file: Annotated[Path, typer.Option('-o', '--output', exists=False, dir_okay=False, writable=True, resolve_path=True, help="EMASE file (hdf5 format)")] = None,
-    delim: Annotated[str, typer.Option('-d', '--delim', help='delimiter string between locus and haplotype in BAM file')] = '_',
-    index_dtype: Annotated[str, typer.Option('--index-dtype', help='advanced option, see internal code')] = 'uint32',
-    data_dtype: Annotated[str, typer.Option('--data-dtype', help='advanced_option, see internal code')] = 'uint8',
-    verbose: Annotated[int, typer.Option('-v', '--verbose', count=True, help="specify multiple times for more verbose output")] = 0
-) -> None:
-    logger = gbrs_utils.configure_logging(verbose)
-    logger.debug('bam2emase')
-    try:
-        # haplotype shortcut: the following command line options are all equal
-        # -h A,B,C,D,E,F,G,H
-        # -h A -h B -h C -h D -h E -h F -h G -h H
-        # -h A,B,C,D -h E -h F -h G,H
-        all_haplotypes: list[str] = []
-        for x in haplotypes:
-            all_haplotypes.extend(x.split(','))
-
-        emase_utils.bam2emase(
-            alignment_file=alignment_file,
-            haplotypes=all_haplotypes,
-            locusid_file=locusid_file,
-            out_file=out_file,
-            delim=delim,
-            index_dtype=index_dtype,
-            data_dtype=data_dtype
-        )
-    except Exception as e:
-        if logger.level == logging.DEBUG:
-            logger.exception(e)
-        else:
-            logger.error(e)
-
-
 @app.command(help="compress EMASE format alignment incidence matrix")
 def compress(
     emase_files: Annotated[list[Path], typer.Option('-i', '--emase-file', exists=True, dir_okay=False, resolve_path=True, help='EMASE file to compress, can seperate files by "," or have multiple -i')],
@@ -355,33 +293,6 @@ def stencil(
             logger.error(e)
 
 
-@app.command()
-def intersect(
-    emase_files: Annotated[list[Path], typer.Option('-i', '--emase-file', exists=True, dir_okay=False, resolve_path=True)],
-    out_file: Annotated[Path, typer.Option('-o', '--output', exists=False, dir_okay=False, writable=True, resolve_path=True)] = None,
-    comp_lib: Annotated[str, typer.Option('-c', '--comp-lib')] = 'zlib',
-    verbose: Annotated[int, typer.Option('-v', '--verbose', count=True, help="specify multiple times for more verbose output")] = 0
-) -> None:
-    logger = gbrs_utils.configure_logging(verbose)
-    logger.debug('intersect')
-    try:
-        # file shortcut: the following command line options are all equal
-        # -i abc.h5 -i def.h5
-        # -i abc.h5,def.h5
-        all_emase_files: list[str] = []
-        for x in emase_files:
-            all_emase_files.extend(str(x).split(','))
-
-        emase_utils.intersect(
-            emase_files=all_emase_files,
-            out_file=out_file,
-            comp_lib=comp_lib
-        )
-    except Exception as e:
-        if logger.level == logging.DEBUG:
-            logger.exception(e)
-        else:
-            logger.error(e)
 
 
 if __name__ == '__main__':
