d50189e35d86a8fd96070b66da4d44893b408bbf
mattjvincent <matt.vincent@jax.org>
2023-05-10 13:20:10 -0400

    Fixed setup.py

--- setup.py
+++ setup.py
@@ -7,9 +7,6 @@ from setuptools import setup, find_packages
 with open('README.rst') as readme_file:
     readme = readme_file.read()
 
-with open('HISTORY.rst') as history_file:
-    history = history_file.read()
-
 
 def get_gbrs_version():
     sys.path.insert(0, "gbrs")
@@ -33,7 +30,7 @@ setup(
     name='gbrs',
     version=get_gbrs_version(),
     description='A suite of tools for Reconstructing Genomes and Quantifying Allele Specific Expression from RNA-Seq data',
-    long_description=readme + '\n\n' + history,
+    long_description=readme,
     author='Kwangbom \"KB\" Choi, Ph.D.',
     author_email='kb.choi@jax.org',
     url='https://github.com/churchill-lab/gbrs',
